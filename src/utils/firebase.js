b463ea9ea9fc74d6e97f0bf2f88d11f33eaf122c
Gungun Kaushik <gungunkaushik.05@gmail.com>
2026-07-15 14:37:52 +0530

    debug firebase env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -11,6 +11,11 @@ const firebaseConfig = {
   appId: "1:870478030038:web:18d3754848cb0aecfc5577",
 };
 
+console.log({
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+});
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
